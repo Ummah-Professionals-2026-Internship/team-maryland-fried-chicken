127acc08ccb0fc6ed047ae228305c06aff1ed22d
oms2304 <oms23@njit.edu>
2026-06-15 15:24:19 -0400

    Added reusabled shadcn card and button components

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,56 @@
 import MainLayout from "@/layouts/MainLayout";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function DashboardPage() {
   return (
     <MainLayout>
-      <h1 className="text-3xl font-bold text-zinc-900">Dashboard</h1>
+      <div className="space-y-4">
+        <h1 className="text-3xl font-bold text-zinc-900">Dashboard</h1>
       <p className="mt-2 text-zinc-600">Placeholder dashboard page.</p>
+      <Card size="sm" className="mx-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Example Card component with Button component </CardTitle>
+        <CardDescription>
+          This card uses the small size variant.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          The card component supports a size prop that can be set to
+          &quot;sm&quot; for a more compact appearance.
+        </p>
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="sm" className="w-full">
+          Button
+        </Button>
+      </CardFooter>
+    </Card>
+
+    <Card size="sm" className="mx-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Example Card component with Button component </CardTitle>
+        <CardDescription>
+          This card uses the small size variant.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          The card component supports a size prop that can be set to
+          &quot;sm&quot; for a more compact appearance.
+        </p>
+      </CardContent>
+    </Card>
+      </div>
     </MainLayout>
   );
 }
