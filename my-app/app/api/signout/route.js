b5bc525e2ef9253bafba9062708128f9e8245153
Shuiab Jimcale <shuiabjimcale1@gmail.com>
2026-07-13 10:53:35 -0400

    Create Sign Out
    Created sign out functionality in the backend using supabase's signOut() method.
    Used global scope to invalidate all active sessions for the user.

---
+++ my-app/app/api/signout/route.js
@@ -0,0 +1,20 @@
+import { createClient } from '@/utils/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function POST(request) {
+    const supabase = createClient()
+
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        )
+    }
+
+    return NextResponse.json(
+        { message: 'Successfully signed out.' },
+        { status: 200 }
+    )
+}
